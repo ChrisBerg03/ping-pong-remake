1a3353410db87e7bfa9e77229a91bf366cb7cc7a
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-01-30 15:55:41 +0100

    it works ishhh

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 let gameState = "start";
-let paddle1 = document.querySelector(".paddle1");
-let paddle2 = document.querySelector(".paddle2");
+let paddle_1 = document.querySelector(".paddle_1");
+let paddle_2 = document.querySelector(".paddle_2");
 let board = document.querySelector(".board");
-let initialBall = document.querySelector(".ball");
+let initial_ball = document.querySelector(".ball");
 let ball = document.querySelector(".ball");
-let score1 = document.querySelector(".player1Score");
-let score2 = document.querySelector(".player2Score");
+let score_1 = document.querySelector(".player_1_score");
+let score_2 = document.querySelector(".player_2_score");
 let message = document.querySelector(".message");
-let paddle1Coord = paddle1.getBoundingClientRect();
-let paddle2Coord = paddle2.getBoundingClientRect();
-let initialBallCoord = ball.getBoundingClientRect();
-let ballCoord = initialBallCoord;
-let boardCoord = board.getBoundingClientRect();
-let paddleCommon = document.querySelector(".paddle").getBoundingClientRect();
+let paddle_1_coord = paddle_1.getBoundingClientRect();
+let paddle_2_coord = paddle_2.getBoundingClientRect();
+let initial_ball_coord = ball.getBoundingClientRect();
+let ball_coord = initial_ball_coord;
+let board_coord = board.getBoundingClientRect();
+let paddle_common = document.querySelector(".paddle").getBoundingClientRect();
 
 let dx = Math.floor(Math.random() * 4) + 3;
 let dy = Math.floor(Math.random() * 4) + 3;
@@ -36,86 +36,86 @@ document.addEventListener("keydown", (e) => {
     }
     if (gameState == "play") {
         if (e.key == "w") {
-            paddle1.style.top =
+            paddle_1.style.top =
                 Math.max(
-                    boardCoord.top,
-                    paddle1Coord.top - window.innerHeight * 0.06
+                    board_coord.top,
+                    paddle_1_coord.top - window.innerHeight * 0.06
                 ) + "px";
-            paddle1Coord = paddle1.getBoundingClientRect();
+            paddle_1_coord = paddle_1.getBoundingClientRect();
         }
         if (e.key == "s") {
-            paddle1.style.top =
+            paddle_1.style.top =
                 Math.min(
-                    boardCoord.bottom - paddleCommon.height,
-                    paddle1Coord.top + window.innerHeight * 0.06
+                    board_coord.bottom - paddle_common.height,
+                    paddle_1_coord.top + window.innerHeight * 0.06
                 ) + "px";
-            paddle1Coord = paddle1.getBoundingClientRect();
+            paddle_1_coord = paddle_1.getBoundingClientRect();
         }
 
         if (e.key == "ArrowUp") {
-            paddle2.style.top =
+            paddle_2.style.top =
                 Math.max(
-                    boardCoord.top,
-                    paddle2Coord.top - window.innerHeight * 0.1
+                    board_coord.top,
+                    paddle_2_coord.top - window.innerHeight * 0.1
                 ) + "px";
-            paddle2Coord = paddle2.getBoundingClientRect();
+            paddle_2_coord = paddle_2.getBoundingClientRect();
         }
         if (e.key == "ArrowDown") {
-            paddle2.style.top =
+            paddle_2.style.top =
                 Math.min(
-                    boardCoord.bottom - paddleCommon.height,
-                    paddle2Coord.top + window.innerHeight * 0.1
+                    board_coord.bottom - paddle_common.height,
+                    paddle_2_coord.top + window.innerHeight * 0.1
                 ) + "px";
-            paddle2Coord = paddle2.getBoundingClientRect();
+            paddle_2_coord = paddle_2.getBoundingClientRect();
         }
     }
 });
 
 function moveBall(dx, dy, dxd, dyd) {
-    if (ballCoord.top <= boardCoord.top) {
+    if (ball_coord.top <= board_coord.top) {
         dyd = 1;
     }
-    if (ballCoord.bottom >= boardCoord.bottom) {
+    if (ball_coord.bottom >= board_coord.bottom) {
         dyd = 0;
     }
     if (
-        ballCoord.left <= paddle1Coord.right &&
-        ballCoord.top >= paddle1Coord.top &&
-        ballCoord.bottom <= paddle1Coord.bottom
+        ball_coord.left <= paddle_1_coord.right &&
+        ball_coord.top >= paddle_1_coord.top &&
+        ball_coord.bottom <= paddle_1_coord.bottom
     ) {
         dxd = 1;
         dx = Math.floor(Math.random() * 4) + 3;
         dy = Math.floor(Math.random() * 4) + 3;
     }
     if (
-        ballCoord.right >= paddle2Coord.left &&
-        ballCoord.top >= paddle2Coord.top &&
-        ballCoord.bottom <= paddle2Coord.bottom
+        ball_coord.right >= paddle_2_coord.left &&
+        ball_coord.top >= paddle_2_coord.top &&
+        ball_coord.bottom <= paddle_2_coord.bottom
     ) {
         dxd = 0;
         dx = Math.floor(Math.random() * 4) + 3;
         dy = Math.floor(Math.random() * 4) + 3;
     }
     if (
-        ballCoord.left <= boardCoord.left ||
-        ballCoord.right >= boardCoord.right
+        ball_coord.left <= board_coord.left ||
+        ball_coord.right >= board_coord.right
     ) {
-        if (ballCoord.left <= boardCoord.left) {
-            score2.innerHTML = +score2.innerHTML + 1;
+        if (ball_coord.left <= board_coord.left) {
+            score_2.innerHTML = +score_2.innerHTML + 1;
         } else {
-            score1.innerHTML = +score1.innerHTML + 1;
+            score_1.innerHTML = +score_1.innerHTML + 1;
         }
         gameState = "start";
 
-        ballCoord = initialBallCoord;
-        ball.style = initialBall.style;
+        ball_coord = initial_ball_coord;
+        ball.style = initial_ball.style;
         message.innerHTML = "Press Enter to Play Pong";
         message.style.left = 38 + "vw";
         return;
     }
-    ball.style.top = ballCoord.top + dy * (dyd == 0 ? -1 : 1) + "px";
-    ball.style.left = ballCoord.left + dx * (dxd == 0 ? -1 : 1) + "px";
-    ballCoord = ball.getBoundingClientRect();
+    ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + "px";
+    ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + "px";
+    ball_coord = ball.getBoundingClientRect();
     requestAnimationFrame(() => {
         moveBall(dx, dy, dxd, dyd);
     });
